726ec63852b809a6dc67bac0677942d7fa60cf92
Artium <awasse@tillersystems.com>
2019-02-08 16:23:54 +0100

    fix(GenericTag): see tag when unlock

--- src/components/DragNDrop/element.js
+++ src/components/DragNDrop/element.js
@@ -5,9 +5,6 @@ export const Svg = styled.svg`
   opacity: 1;
   transition: all 1s ease-out;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
   z-index: 10;
   top: ${({ top }) => top};
   left: ${({ left }) => left};
