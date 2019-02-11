be96db3485365af636398f132305703986893315
audrenmp <audren.mp@gmail.com>
2019-02-11 12:21:26 +0100

    style point d'interet

--- src/components/GenericTag/element.js
+++ src/components/GenericTag/element.js
@@ -66,7 +66,7 @@ export const Paragraph = styled.div`
   color: #000;
   font-size: 14px;
   padding: 15px 15px;
-  line-height: 2em;
+  line-height: 1.8em;
   max-height: 260px;
   overflow: auto;
 
