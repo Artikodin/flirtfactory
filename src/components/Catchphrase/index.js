a1554b4e8c01015381a835d30db44ade7fa76358
Artium <awasse@tillersystems.com>
2019-02-14 18:42:27 +0100

    fix error on catchphrase

--- src/components/Catchphrase/index.js
+++ src/components/Catchphrase/index.js
@@ -4,7 +4,6 @@ import { withRouter } from "react-router-dom";
 
 import PropTypes from "prop-types";
 import posed, { PoseGroup } from "react-pose";
-import { easing } from "popmotion";
 
 import { TextContext, Picto } from "../index";
 
